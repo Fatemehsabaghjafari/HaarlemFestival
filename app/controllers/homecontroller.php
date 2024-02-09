21092f6b317d6413aa95ce2ed3b4669082d1fe19
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2024-02-09 13:51:45 +0100

    .

---
+++ app/controllers/homecontroller.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/controller.php';
+
+class HomeController extends Controller
+{
+   
+
+    public function __construct()
+    {
+       
+    }
+
+    public function index()
+    {
+     
+    }
+}
