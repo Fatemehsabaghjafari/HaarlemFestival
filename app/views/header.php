0a639f8c38bcba96ba0d43c13a4a3634537e0e7e
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2024-02-19 20:22:35 +0100

    .

--- app/views/header.php
+++ app/views/header.php
@@ -11,7 +11,7 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img src="img/Logo.png" alt="Logo" class="logo"></a>
+        <a class="navbar-brand" href="/"><img src="img/Logo.jpeg" alt="Logo" class="logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -26,7 +26,7 @@
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <ul>
-                  <li><a class="dropdown-item" href="#">ART & CULTURE</a></li>
+                  <li><a class="dropdown-item" href="/artCulture">ART & CULTURE</a></li>
                   <li><a class="dropdown-item" href="#">HISTORY</a></li>
                   <li><a class="dropdown-item" href="#">HAARLEM FOR FOODIES</a></li>
                 </ul>
@@ -55,10 +55,10 @@
           </ul>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="NL.png" alt="Logo" style="max-height: 30px;">
+              <img src="/img/NL.jpeg" alt="Logo" style="max-height: 30px;">
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <img src="img/NL.png" alt="Logo" style="max-height: 30px;">
+              <img src="/img/NL.jpeg" alt="Logo" style="max-height: 30px;">
             </ul>
           </li>
         </div>
@@ -66,7 +66,11 @@
     </nav>
     
 
+
+   
     
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
