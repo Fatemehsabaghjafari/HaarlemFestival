794c046ec0b216b25c1a31d3e8e02783d5298980
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2024-07-09 03:26:42 +0200

    Revert "Merge branch 'Header&Footer'"
    This reverts commit a1bf987edc8b84029310ac702f6b6a467f6aa09b, reversing
    changes made to 825c95c3171d08d9a3776302dd2e36a196591616.

--- app/views/index.php
+++ app/views/index.php
@@ -37,9 +37,6 @@
             <h3>YUMMY</h3>
         </div>
     </div>
-   
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
- </body>
 
     <div class="navigation-card-container">
         <div class="navigation-card-backdrop"></div>
